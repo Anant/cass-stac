08ed77944cd92792bb9f79c0e7d43038126d1fe6
jsbilgi <jsbilgi@yahoo.com>
2024-08-10 13:35:19 +0530

    Removed docker build command

--- setup_devcontainer.sh
+++ setup_devcontainer.sh
@@ -23,14 +23,3 @@ echo "ASTRA_DB_KEYSPACE=$ASTRA_DB_KEYSPACE" >> .env
 echo "ASTRA_DB_ID=$ASTRA_DB_ID" >> .env
 echo "DATASTAX_ASTRA_PASSWORD=$DATASTAX_ASTRA_PASSWORD" >> .env
 echo "DATASTAX_ASTRA_SCB_NAME=$DATASTAX_ASTRA_SCB_NAME" >> .env
-
-# Build the Docker image without using cache
-docker build -t stac-app \
-  --build-arg ASTRA_DB_USERNAME=$ASTRA_DB_USERNAME \
-  --build-arg ASTRA_DB_KEYSPACE=$ASTRA_DB_KEYSPACE \
-  --build-arg ASTRA_DB_ID=$ASTRA_DB_ID \
-  --build-arg DATASTAX_ASTRA_PASSWORD=$DATASTAX_ASTRA_PASSWORD \
-  --build-arg DATASTAX_ASTRA_SCB_NAME=$DATASTAX_ASTRA_SCB_NAME \
-  .
-
-
